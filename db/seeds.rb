77f48cd4fd65e75e23742ff392468422b8a271c5
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2023-12-31 15:40:50 +0800

    [MG] Authenticate User with Devise Gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Create User
+User.create(
+  name: 'KiyoScript',
+  email: 'KiyoScript@gmail.com',
+  username: 'kiyoscript',
+  bio: 'Life is not a race but a life.',
+  password: '123123123',
+  password_confirmation: '123123123'
+)
