7e54b2a86ecd8bbcce788225686fe754757e32cd
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-01-04 23:04:41 +0800

    [MG] Remove Other User's Fields

--- db/migrate/20231231084709_devise_create_users.rb
+++ db/migrate/20231231084709_devise_create_users.rb
@@ -6,9 +6,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :username,           null: true, default: ""
-      t.string :bio,                null: true, default: ""
-      t.string :name,               null: true, default: ""
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -36,9 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_01_135700) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "username", default: ""
-    t.string "bio", default: ""
-    t.string "name", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
